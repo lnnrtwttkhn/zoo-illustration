988d3f728a6921903da2bc26ad239199cedf672b
Lennart Wittkuhn <wittkuhn@mpib-berlin.mpg.de>
2021-10-14 11:47:57 +0200

    add illustration for training procedure, add titles for each panel, minor fixes

--- code/zoo-task-procedure.R
+++ code/zoo-task-procedure.R
@@ -13,15 +13,16 @@ make_fig = function(path) {
   return(fig)
 }
 
+fig_training = make_fig(Sys.glob(file.path(path_illustration, "*task*training*.png")))
 fig_recall = make_fig(Sys.glob(file.path(path_illustration, "*task*recall*.png")))
 fig_graph = make_fig(Sys.glob(file.path(path_illustration, "*task*graph*.png")))
-fig_task = fig_recall / fig_graph +
+fig_task = fig_training / fig_recall / fig_graph +
   patchwork::plot_annotation(tag_levels = "a") &
   theme(plot.tag = element_text(face = "bold"))
 
 ggsave(file.path(path_output, "zoo_task_procedure.pdf"),
        device = "pdf", dpi = "retina",
-       fig_task, width = 6, height = 4)
+       fig_task, width = 6, height = 6)
 ggsave(file.path(path_output, "zoo_task_procedure.png"),
        device = "png", dpi = "retina",
-       fig_task, width = 6, height = 4)
+       fig_task, width = 6, height = 6)
