d265ed158dfe3835abd678a57ef5bf3d280f1c0b
Lennart Wittkuhn <wittkuhn@mpib-berlin.mpg.de>
2021-11-09 14:22:00 +0100

    add graph trial to graph figure and add response buttons to trial procedure

--- code/zoo-task-illustration.R
+++ code/zoo-task-illustration.R
@@ -10,15 +10,18 @@ path_transmat = Sys.glob(here::here("output", "transition_matrix", "*transition_
 stopifnot(length(path_transmat) == 1)
 path_graphstruct = Sys.glob(here::here("output", "graph_structure", "*graph_structure.png"))
 stopifnot(length(path_graphstruct) == 1)
+path_graphtrial = Sys.glob(here::here("output", "illustration", "*task*graph*.png"))
+stopifnot(length(path_graphtrial) == 1)
 path_output = here::here("output", "task_illustration")
 dir.create(path_output, showWarnings = FALSE)
 
 fig_graphproc = make_figure(path_graphproc)
 fig_transmat = make_figure(path_transmat)
 fig_graphstruct = make_figure(path_graphstruct)
+fig_graphtrial = make_figure(path_graphtrial)
 
-fig_task = fig_graphstruct / fig_transmat / (fig_graphproc + plot_spacer()) +
-  patchwork::plot_layout(nrow = 3) + 
+fig_task = fig_graphstruct / fig_transmat / (fig_graphproc + fig_graphtrial) +
+  patchwork::plot_layout(nrow = 3) +
   patchwork::plot_annotation(tag_levels = "a") &
   theme(plot.tag = element_text(face = "bold")) &
   theme(plot.margin = unit(c(2, 2, 2, 2), "pt"))
--- code/zoo-task-procedure.R
+++ code/zoo-task-procedure.R
@@ -4,10 +4,12 @@ path_illustration = here::here("output", "illustration")
 path_output = here::here("output", "task_procedure")
 dir.create(path_output, showWarnings = FALSE)
 
+fig_response = make_figure(Sys.glob(file.path(path_illustration, "*response_buttons.png")))
 fig_training = make_figure(Sys.glob(file.path(path_illustration, "*task*training*.png")))
 fig_recall = make_figure(Sys.glob(file.path(path_illustration, "*task*recall*.png")))
 fig_graph = make_figure(Sys.glob(file.path(path_illustration, "*task*graph*.png")))
-fig_task = fig_training / fig_recall / fig_graph +
+fig_task = fig_response / fig_training / fig_recall +
+  patchwork::plot_layout(widths = c(1, 1, 1)) +
   patchwork::plot_annotation(tag_levels = "a") &
   theme(plot.tag = element_text(face = "bold"))
 
